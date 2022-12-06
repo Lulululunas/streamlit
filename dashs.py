223de30246f1348c75241a7ceaa7609a7d7b49fd
Lulululunas <90847523+Lulululunas@users.noreply.github.com>
2022-12-06 23:23:42 +0300

    Update dashs.py

--- dashs.py
+++ dashs.py
@@ -14,7 +14,6 @@ import plotly.express as px
 
 
 df = pd.read_csv('crimedata.csv', sep=',')
-df.head(4)
 
 
 # In[ ]:
@@ -26,3 +25,23 @@ st.sidebar.title("About")
 st.sidebar.info(" The github link can be found "
                 "[here](https://github.com/Lulululunas/streamlit/blob/main/dashs.py)")
 
+
+crime = ['murders', 'rapes', 'robberies', 'assaults', 'arsons', 'autoTheft', 'larcenies', 'burglaries']
+
+def state_crimes(crimes = crime):
+    df_states = df[crime + ['state']].groupby(['state']).sum().reset_index()
+    for state in states:
+        x = df_states.loc[state][name_x]
+        y = df_states.loc[state][name_y]
+        data_states.append(go.Scatter(x=x, y=y, mode = 'markers'))
+    
+    layout = go.Layout(
+        width=1000,
+        height=600
+    )
+
+    plot = go.Figure(data = df_states, layout = layout)
+    return plot
+  
+select_crime = st.sidebar.selectbox('Select crime', ('murders', 'rapes', 'robberies', 'assaults', 'arsons', 'autoTheft', 'larcenies', 'burglaries'))
+st.plotly_chart(state_crimes(select_crime), use_container_width=True)  
