f4b74fddc08063e2c8a60c608c12a9e689a57e36
Lulululunas <90847523+Lulululunas@users.noreply.github.com>
2022-12-07 15:26:26 +0300

    Create ages.py

---
+++ pages/ages.py
@@ -0,0 +1,35 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import plotly.express as px
+import plotly.graph_objs as go
+
+
+df = pd.read_csv('crimedata.csv', sep=',')
+df.dropna(inplace = True)
+
+# Настройка боковой панели
+st.sidebar.title("Crimes and income")
+
+age = ['12 to 21', '12 to 29', '16 to 24', 'above 60']
+
+df_new = df[['ViolentCrimesPerPop', 'agePct12t21', 'agePct12t29', 'agePct16t24', 'agePct65up']].copy()
+
+df_new = df_new.set_axis(['ViolentCrimes']+ages, axis=1, copy=False)
+
+def ages_crimes(ages):
+    if ages == 'all':
+        st.title("Violent crimes and ages")
+        st.write("""How violent crimes per capita correlate with ages above 12""")
+        fig = px.scatter(x=df_new['ViolentCrimes'].values, y=df_new[age].sum(axis=1).values, trendline="ols")
+    else:
+        st.title("Violent crimes and ages "+ages)
+        st.write("""How violent crimes per capita correlate with ages""", ages)
+        fig = px.scatter(x=df_new['ViolentCrimes'].values, y=df_new[ages].values, trendline="ols")
+    fig.update_layout(xaxis_title="Violent crimes per capita",
+                  yaxis_title="Percentage of the population")
+    fig.update_traces(marker_size=8)
+    return fig
+
+select_crime = st.sidebar.selectbox('Select:', ('above 12', '12 to 21', '12 to 29', '16 to 24', 'above 60'))
+st.plotly_chart(ages_crimes(select_crime), use_container_width=True)
