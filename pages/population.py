ceadd6222b435037568833de168d9b9cd1cf2eea
Lulululunas <90847523+Lulululunas@users.noreply.github.com>
2022-12-07 13:44:05 +0300

    Update population.py

--- pages/population.py
+++ pages/population.py
@@ -15,7 +15,7 @@ df = pd.read_csv('crimedata.csv', sep=',')
 df.dropna(inplace = True)
 
 st.title("Crimes and population")
-st.write("""How crimes correlate with population""")
+# st.write("""How crimes correlate with population""")
 
 # Настройка боковой панели
 st.sidebar.title("Crimes and population")
@@ -23,9 +23,11 @@ st.sidebar.title("Crimes and population")
 
 def population_crimes(crimes):
     if crimes == 'all':
+        st.write("""How crimes correlate with population""")
         crime = ['murders', 'rapes', 'robberies', 'assaults', 'arsons', 'autoTheft', 'larcenies', 'burglaries']
         fig = px.scatter(x=df['population'].values, y=df[crime].sum(axis=1).values, trendline="ols")
     else:
+        st.write("""How""", crimes, """correlate with population""")
         df_pop = df[[crimes] + ['population']]
         fig = px.scatter(x=df_pop['population'].values, y=df_pop[crimes].values, trendline="ols")
     fig.update_layout(xaxis_title="Number of population",
